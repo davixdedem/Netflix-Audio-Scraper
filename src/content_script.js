bffac9518648414abaad5413a55e06c3d33e3034
davixdedem <davide.polli@dedem.it>
2023-04-07 00:15:06 +0200

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch main # Your branch is up to date with 'origin/main'. # # Changes to be committed: #	deleted:    src/content_key_decryption.js #	modified:   src/content_script.js #	deleted:    src/docs/WidevineModularDRMSecurityIntegrationGuideforCENC.pdf #	deleted:    src/docs/Widevine_DRM_Architecture_Overview.pdf #	deleted:    src/eme_interception.js #	deleted:    src/lib/cryptojs-aes_0.2.0.min.js #	deleted:    src/lib/pbf.3.0.5.min.js #	deleted:    src/license_protocol.proto #

--- src/content_script.js
+++ src/content_script.js
@@ -2,15 +2,7 @@ injectScripts();
 
 async function injectScripts() 
 {
-    // await injectScript('lib/pbf.3.0.5.min.js');
-    // await injectScript('lib/cryptojs-aes_0.2.0.min.js');
-    // await injectScript('protobuf-generated/license_protocol.proto.js');
-
-
-    // await injectScript('content_key_decryption.js');
-    // await injectScript('eme_interception.js');
     await injectScript('lib/cadmium-playercore-6.0039.582.911.js');
-    // await injectScript('lib/akiraClient.js.330fa72168f2bc5c9730.js');
 }
 
 function injectScript(scriptName) 
